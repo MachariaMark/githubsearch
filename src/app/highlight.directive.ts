7c9da87ea49a758c5842c5c5b669e8ebfb650f3b
1offmark <official1offmark@gmail.com>
2021-08-28 23:54:14 +0300

    fixed bug in highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,16 +6,18 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 export class HighlightDirective {
   constructor(private el: ElementRef) { }
 
-  @Input() defaultColor: string;
+  @Input()
+  defaultColor!: string;
 
-  @Input('appHighlight') highlightColor: string;
+  @Input('appHighlight')
+  highlightColor!: string;
 
   @HostListener('mouseenter') onMouseEnter() {
     this.highlight(this.highlightColor || this.defaultColor || 'red');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight(null);
+    this.highlight;
   }
 
   private highlight(color: string) {
