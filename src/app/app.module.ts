60c568897aedf0e597fd6551a49c96b8aa4232b3
1offmark <official1offmark@gmail.com>
2021-08-29 01:18:00 +0300

    changed directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AboutComponent } from './about/about.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { HighlightDirective } from './highlight.directive';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { DateTrackPipe } from './pipes/date-track.pipe';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { HighlightDirective } from './highlight.directive';
     AboutComponent,
     ProfileComponent,
     SearchFormComponent,
-    HighlightDirective
+    HighlightDirective,
+    DateTrackPipe
   ],
   imports: [
     BrowserModule,
